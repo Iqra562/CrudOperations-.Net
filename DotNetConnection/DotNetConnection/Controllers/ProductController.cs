a96ff82302fe4a09ce90a00ff4871f84909d4117
Iqra562 <iqra44938@gmail.com>
2023-11-04 16:14:51 +0500

    PRODUCTADDED

---
+++ DotNetConnection/DotNetConnection/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using DotNetConnection.Data;
+using DotNetConnection.Models;
+using DotNetConnection.Models.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetConnection.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Products.Include(e => e.Category));
+        }
+        public IActionResult Create()
+        {
+            ViewBag.Categories = _db.Categories;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ProductRegisterRequest request)
+        {
+            ViewBag.Categories = _db.Categories;
+            if (ModelState.IsValid)
+            {
+                Product p = new Product();
+                p.Name = request.Name;
+                p.Price = request.Price;
+                p.CategoryId = request.CategoryId;
+                p.Image = "";
+                _db.Products.Add(p);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+
+
+            }
+            return View(request);
+
+
+        }
+    }
+}
---
+++ DotNetConnection/DotNetConnection/Models/Forms/ProductRegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetConnection.Models.Forms
+{
+    public class ProductRegisterRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Price { get; set; }
+       // [Required]
+       // public string Image { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
